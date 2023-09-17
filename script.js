853a24c860061f681451789568406c565bd8ca5c
JoaquimSP <joaquimsp2000@gmail.com>
2023-09-17 19:39:36 -0300

    🗂️ feat: uploading the files

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addGoal() {
+    if(inputBox.value === '') {
+        alert("Você precisa escrever algum objetivo!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveGoal();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if(e.target.tagName === "LI") {
+         e.target.classList.toggle("checked");
+         saveGoal();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveGoal();
+    }
+}, false);
+
+function saveGoal() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showGoal() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showGoal();
